442245675c13d8f1bd5322f66df9540601ef3720
vasundharaD <vasundharadehiya@gmail.com>
2017-11-20 12:12:40 +0900

    Initial commit

---
+++ translate.R
@@ -0,0 +1,47 @@
+translate <-
+function(dataset = NULL, content.field = NULL, content.vec = NULL,
+                      google.api.key = NULL, microsoft.api.key = NULL,
+                      source.lang = NULL, target.lang = NULL, RCurlOptions = NULL){
+
+    if(!is.null(RCurlOptions)){
+        options(RCurlOptions = RCurlOptions)
+    }
+    # Do some sanity checking
+    translator <- validateInput(dataset, content.field, content.vec, google.api.key,
+                                microsoft.api.key,
+                                source.lang, target.lang)
+    
+    # Get translation vector
+    if(!(is.null(dataset))){
+        to.translate <- dataset[[content.field]]
+    }
+    if(!(is.null(content.vec))){
+        to.translate <- content.vec
+    }
+    
+    # Do the translation
+    if(translator == 'Google'){
+        translated <- unname(
+            unlist(
+                mclapply(to.translate, function(x) googleTranslate(x, google.api.key, source.lang, target.lang))
+                )
+            )
+    }
+
+    if(translator == 'Microsoft'){ 
+        translated <- unname(
+          unlist(
+            mclapply(to.translate, function(x) microsoftTranslate(x, microsoft.api.key, source.lang, target.lang))
+          )
+        )
+    }
+
+    # Figure out what we should return
+    if(!(is.null(content.vec))){
+        return(translated)
+    }
+    if(!(is.null(dataset) & is.null(content.field))){
+        dataset$translatedContent <- translated
+        return(dataset)
+    }
+}
